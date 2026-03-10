d0078f0644f7fead6f5da1b740b89efe124518f3
Fabio Manganiello <fabio@manganiello.tech>
2026-03-11 00:29:37 +0100

    Bump version: 0.2.5 → 0.2.6

--- src/python/pubby/__init__.py
+++ src/python/pubby/__init__.py
@@ -22,7 +22,7 @@ from ._model import (
 from ._rate_limit import RateLimiter
 from .webfinger import Mention, extract_mentions, resolve_actor_url
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 
 __all__ = [
     "Activity",
