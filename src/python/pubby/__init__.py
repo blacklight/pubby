96630660b2766aa260751fe7a439a2a3e6525f66
Fabio Manganiello <fabio@manganiello.tech>
2026-09-24 01:30:11 +0200

    Bump version: 0.3.9 → 0.3.10

--- src/python/pubby/__init__.py
+++ src/python/pubby/__init__.py
@@ -64,7 +64,7 @@ from .quotes import (
 from .storage import ActivityPubStorage
 from .webfinger import Mention, extract_mentions, resolve_actor_url
 
-__version__ = "0.3.9"
+__version__ = "0.3.10"
 
 __all__ = [
     "Activity",
